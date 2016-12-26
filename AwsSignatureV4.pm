c2553fd10f690b9a135b16356cd7c3e76e0842c3
Marcus Hughes <hello@entirely.pro>
2016-12-26 16:39:10 +0000

    Add NTP date within signature

--- AwsSignatureV4.pm
+++ AwsSignatureV4.pm
@@ -22,6 +22,9 @@ use URI::Escape qw(uri_escape_utf8);
 # Package Digest::SHA is implemnted in C for performance.
 use Digest::SHA qw(sha256_hex hmac_sha256 hmac_sha256_hex);
 
+use IO::Socket;
+use POSIX;
+
 # For using PurePerl implementation of SHA functions
 # use Digest::SHA::PurePerl qw(sha256_hex hmac_sha256 hmac_sha256_hex);
 
@@ -291,11 +294,53 @@ sub error
 sub get_timestamp_daystamp
 {
   my $time = shift;
-  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = gmtime(time);
-  my $timestamp = sprintf("%04d%02d%02dT%02d%02d%02dZ", $year + 1900, $mon + 1, $mday, $hour, $min, $sec);
+  my $timestamp = GetNTPTime('proxy');
   my $daystamp = substr($timestamp, 0, 8);
   return ($timestamp, $daystamp);
 }
+
+sub get_timestamp
+{
+  my $time = shift;
+  sprintf("%04d%02d%02dT%02d%02d%02dZ",
+    sub {($_[5]+1900,$_[4]+1,$_[3],$_[2],$_[1],$_[0])}->(gmtime($time)));
+}
+
+sub GetNTPTime{
+  my $host = shift;
+  my $timeout = 2;
+  my $sock = IO::Socket::INET->new(
+    Proto => 'udp',
+    PeerPort => 123,
+    PeerAddr => $host,
+     Timeout => $timeout
+  ) or do {warn "Can't contact $host\n"; return 0};
+  my $ntp_msg = pack("B8 C3 N11", '00100011', (0)x14);
+  $sock->send($ntp_msg) or do {warn "Can't send NTP msg."; return 0};
+  my $rin='';
+  vec($rin, fileno($sock), 1) = 1;
+  select(my $rout=$rin, undef, my $eout=$rin, $timeout) or do {warn "No answer from $host"; return 0};
+  $sock->recv($ntp_msg, length($ntp_msg))
+    or do {warn "Receive error from $host: ($!)"; return 0};
+  my ($LIVNMode, $Stratum, $Poll, $Precision,
+      $RootDelay, $RootDispersion, $RefIdentifier,
+      $Reference, $ReferenceF, $Original, $OriginalF,
+      $Receive, $ReceiveF, $Transmit, $TransmitF
+    ) = unpack('a C2 c1 N8 N N B32', $ntp_msg);
+  $Receive -= 2208988800;
+  my $NTPTime = $Receive + $ReceiveF / 65536 / 65536;
+  $RefIdentifier = unpack('A4', $RefIdentifier);
+  my $LI = vec($LIVNMode, 3, 2);
+  my $VN = unpack("C", $LIVNMode & "\x38") >> 3;
+  my $Mode = unpack("C", $LIVNMode & "\x07");
+  $sock->close;
+  if ($RefIdentifier ne '1280') {
+    return get_timestamp(ceil($NTPTime))
+  } else {
+    return 0
+  }
+}
+
 
 #
 # Applies regex to get FQDN from URL.
